31137c183a006d48f6743d3e8d9546d54f2f5051
LolloLauz <94393135+LolloLauz@users.noreply.github.com>
2022-01-13 17:52:07 +0100

    Implementazione di login

--- src/main/java/com/example/progettocasotto/View/ICliente.java
+++ src/main/java/com/example/progettocasotto/View/ICliente.java
@@ -1,35 +1,84 @@
 package com.example.progettocasotto.View;
 
 import com.example.progettocasotto.Controller.DefaultUserController;
+import com.example.progettocasotto.Controller.DefaultaMasterController;
 
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 
 
-
-public class UsersView {
+public class ICliente {
    DefaultUserController userController;
+   DefaultaMasterController masterController;
 
-    public UsersView(DefaultUserController userController) {
+
+    public ICliente(DefaultUserController userController, String currentUser, DefaultaMasterController masterController) {
         this.userController = userController;
-        provaMetodi();
+        this.masterController=masterController;
+        userController.setlistaClienti(masterController.getListaClienti());
+        userController.setCurrentClient(currentUser);
+        Scanner scanner=new Scanner(System.in);
+        int i=scanner.nextInt();
+        switch (i){
+            case 1:
+                prenotaOmbrellone();
+                break;
+            case 2:
+                prenotaSdraio();
+                break;
+            case 3:
+                prenotaAttivita();
+                break;
+            case 4:
+                ordinaBibita();
+                break;
+            default:
+                break;
+        }
     }
-    
 
-    public void provaMetodi(){
+    private boolean ordinaBibita() {
+        return false;
+    }
+
+    private boolean prenotaAttivita() {
+        return false;
+    }
+
+    private boolean prenotaSdraio() {
+        return false;
+    }
+
+    private boolean prenotaOmbrellone() {
         Date dataInizio;
         Date dataFine;
         DateFormat dateFormat=DateFormat.getDateInstance(DateFormat.SHORT, Locale.ITALY);
         try {
-            dataInizio= dateFormat.parse("10/06/2020");
-            dataFine=dateFormat.parse("12/06/2020");
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("INSERIRE LA DATA di inizio");
+            dataInizio=dateFormat.parse(scanner.nextLine());
+            System.out.println("Inserire la data di fine");
+            dataFine=dateFormat.parse(scanner.nextLine());
+            userController.mostraStatoSpiaggia(dataInizio,dataFine);
             userController.prenotaOmbrellone(dataInizio,dataFine);
+            System.out.println("Seleziona ombrellone");
+            String check=scanner.nextLine();
+            while(check!=""){
+                System.out.println("Seleziona ombrellone");
+                userController.selectOmbrellone(check);
+                check=scanner.nextLine();
+            }
+            userController.confermaPernotazione();
+            return true;
+//            userController.prenotaOmbrellone(dataInizio,dataFine);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        userController.StampaSpiaggia();
+        return false;
     }
+
 }
