a228347c953ccbbf2123b9cec113f9f6391852ca
matteomarca99 <matteo.marca99@gmail.com>
2022-01-29 15:50:33 +0100

    Update Personale Interface

--- src/main/java/com/example/progettocasotto/Controller/PersonaleInterface.java
+++ src/main/java/com/example/progettocasotto/Controller/PersonaleInterface.java
@@ -5,10 +5,21 @@ import com.example.progettocasotto.DataBase.Privilegio;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Interfaccia che definisce il personale della spiaggia.
+ */
 public interface PersonaleInterface {
+
+    /**
+     * Metodo usato per creare un utente nel sistema.
+     * @param nome il nome dell'utente.
+     * @param cognome il cognome dell'utente.
+     * @param email l'email dell'utente.
+     * @param password la password dell'utente.
+     * @param privilegio il privilegio dell'utente.
+     * @return true se
+     */
     boolean creaUtente(String nome, String cognome, String email, String password, Privilegio privilegio);
-
-
 
     void modificaPrenotazione(String idPrenotazione, Date dataInizio, Date dataFine, ArrayList<String> ombrelloni,int numSdraio);
 
