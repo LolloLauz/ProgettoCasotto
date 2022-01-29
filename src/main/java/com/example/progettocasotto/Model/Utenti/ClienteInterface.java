fcb5006cdb17796d4184183fcf938098e28410e9
matteomarca99 <matteo.marca99@gmail.com>
2022-01-29 17:06:21 +0100

    Implementazione metodi di ClienteInterface e aggiunta degli ultimi javadoc sulle interfacce.

--- src/main/java/com/example/progettocasotto/Model/Utenti/ClienteInterface.java
+++ src/main/java/com/example/progettocasotto/Model/Utenti/ClienteInterface.java
@@ -1,4 +1,16 @@
 package com.example.progettocasotto.Model.Utenti;
 
+import com.example.progettocasotto.Model.Spiaggia.DefaultPrenotazione;
+
+/**
+ * Interfaccia che definisce un cliente.
+ */
 public interface ClienteInterface {
+
+    /**
+     * Metodo usato per aggiungere una prenotazione ad un cliente.
+     * @param prenotazione
+     * @return true se l'aggiunta è avvenuta con successo, false altrimenti.
+     */
+    boolean addPrenotazioneToCliente(DefaultPrenotazione prenotazione);
 }
